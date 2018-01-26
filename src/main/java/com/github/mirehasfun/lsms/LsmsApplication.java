6f173dbca355d65e41ddbb5e394a1ccaab644f0c
mirehasfun <mirehasfun@users.noreply.github.com>
2018-01-26 12:56:15 +0100

    Initial commit with project files from Spring Initializr

---
+++ src/main/java/com/github/mirehasfun/lsms/LsmsApplication.java
@@ -0,0 +1,12 @@
+package com.github.mirehasfun.lsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LsmsApplication.class, args);
+	}
+}
