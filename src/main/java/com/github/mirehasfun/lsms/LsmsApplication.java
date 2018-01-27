f14a6683802f84aabf2868fc2c64c9363276cfb2
mirehasfun <mirehasfun@users.noreply.github.com>
2018-01-27 22:39:33 +0100

    JavaDoc and code reformatting.

--- src/main/java/com/github/mirehasfun/lsms/LsmsApplication.java
+++ src/main/java/com/github/mirehasfun/lsms/LsmsApplication.java
@@ -3,10 +3,13 @@ package com.github.mirehasfun.lsms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The LongestStringMicroService Application class.
+ */
 @SpringBootApplication
 public class LsmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LsmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LsmsApplication.class, args);
+    }
 }
